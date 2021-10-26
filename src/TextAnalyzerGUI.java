fc705517da25ffa5fb2a34db363d5f24a1473daa
Jen <jmiller283@mail.valenciacollege.edu>
2021-10-25 23:12:29 -0400

    add layout and buttons

--- src/TextAnalyzerGUI.java
+++ src/TextAnalyzerGUI.java
@@ -1,11 +1,16 @@
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
 public class TextAnalyzerGUI {
-
 	private JFrame frame;
-
 	/**
 	 * Launch the application.
 	 */
@@ -21,21 +26,55 @@ public class TextAnalyzerGUI {
 			}
 		});
 	}
-
 	/**
 	 * Create the application.
 	 */
 	public TextAnalyzerGUI() {
 		initialize();
 	}
-
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		// window
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 810, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		// header
+		JLabel lblNewLabel = new JLabel("Please select an action choice: ");
+		lblNewLabel.setBounds(0, 0, 786, 24);
+		lblNewLabel.setFont(new Font("Times New Roman", Font.BOLD, 20));
+		frame.getContentPane().add(lblNewLabel);
+		// view poem
+		JButton btnNewButton = new JButton("View Poem");
+		btnNewButton.setBounds(0, 150, 265, 170);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnNewButton.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		frame.getContentPane().add(btnNewButton);
+		// poem word frequency
+		JButton btnNewButton_1 = new JButton("Word Frequency Count (Entire Poem)");
+		btnNewButton_1.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnNewButton_1.setBounds(265, 150, 265, 170);
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		frame.getContentPane().add(btnNewButton_1);
+		// one word frequency
+		JButton btnNewButton_2 = new JButton("Word Frequency Count (One Word)");
+		btnNewButton_2.setFont(new Font("Times New Roman", Font.PLAIN, 12));
+		btnNewButton_2.setBounds(530, 150, 265, 170);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		frame.getContentPane().add(btnNewButton_2);
 	}
-
 }
